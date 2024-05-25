c05621770ad5e453300522893d1ccb703097454f
asadullah48 <asadullahshafique@hotmail.com>
2024-05-25 11:30:36 +0500

    Initial commit

---
+++ ans75.ts
@@ -0,0 +1,22 @@
+// Q 75
+// This function uses compound assignment operators for different operations
+function useCompoundOperators() {
+    let x = 41; // Starts with x equal to 4
+    console.log("Initial x:", x);
+  
+    x += 21; // Adds 21 to x
+    console.log("After addition:", x); // Shows x after addition
+  
+    x -= 11; // Subtracts 11 from x
+    console.log("After subtraction:", x); // Shows x after subtraction
+  
+    x *= 31; // Multiplies x by 31
+    console.log("After multiplication:", x); // Shows x after multiplication
+  
+    x /= 21; // Divides x by 21
+    console.log("After division:", x); // Shows x after division
+  }
+  
+  useCompoundOperators();
+  // We simplify arithmetic operations on x 
+  //using compound assignment operators, like a shortcut.
